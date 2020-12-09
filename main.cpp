5490d4001a9e629a083c44abe4c8f970bebc3d5e
tuyetlinhnguyen <60365022+tuyetlinhnguyen@users.noreply.github.com>
2020-12-09 12:35:01 -0500

    updated main
    with new "example" function calls

--- main.cpp
+++ main.cpp
@@ -2,14 +2,26 @@
 #include "sort.h"
 
 int main(int argc, char *argv[]){
+    // checks for correct input - outputs instructions
     if (argc != 2) {
-        std::cout << "Usage: ./sort <sortName\n";
-        std::cout << "\t<sortName\talgorithm to be used (insert, merge, quickHi, quickMid, radix)\n";
+        std::cout << "To write CSV datafile:\n\t";
+        std::cout << "Usage: ./sort <sortName>\n";
+        std::cout << "\t\t<sortName>: algorithm to be used (insert, merge, quick, radix)\n";
+
+        std::cout << "To see an example:\n\t";
+        std::cout << "Usage: ./sort <sortNameEx>\n";
+        std::cout << "\t\t<sortNameEx>: algorithm to be used (insertEx, mergeEx, quickEx, radixEx)\n";
         return 0;
     }
+    
     std::string alg(argv[1]);
 
     Sorter name;
-    name.sort(alg);
-
+    
+    // write to CSV file or shows an example
+    if (alg == "insert" || alg == "merge" || alg == "quick" || alg == "radix"){
+         name.sort(alg);
+    }else{ // alg == <sort>Ex
+        name.example(alg);
+    }
 }
