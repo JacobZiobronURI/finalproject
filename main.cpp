251b7a8f05882f4a4f27dce5d9ff5e6939b0cfb8
tuyetlinhnguyen <60365022+tuyetlinhnguyen@users.noreply.github.com>
2020-12-09 13:09:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,25 +3,30 @@
 
 int main(int argc, char *argv[]){
     // checks for correct input - outputs instructions
-    if (argc != 2) {
+    if (argc != 2 && argc != 3) {
         std::cout << "To write CSV datafile:\n\t";
         std::cout << "Usage: ./sort <sortName>\n";
         std::cout << "\t\t<sortName>: algorithm to be used (insert, merge, quick, radix)\n";
 
         std::cout << "To see an example:\n\t";
-        std::cout << "Usage: ./sort <sortNameEx>\n";
+        std::cout << "Usage: ./sort <sortNameEx> <seqType>\n";
         std::cout << "\t\t<sortNameEx>: algorithm to be used (insertEx, mergeEx, quickEx, radixEx)\n";
+        std::cout << "\t\t<seqType>: sequence type to be generated (sorted, reverse, random, partial)\n";
         return 0;
     }
-    
+
     std::string alg(argv[1]);
+    std::string seq;
+    if (argc == 3){
+        seq = argv[2];
+    }
 
     Sorter name;
-    
+
     // write to CSV file or shows an example
-    if (alg == "insert" || alg == "merge" || alg == "quick" || alg == "radix"){
-         name.sort(alg);
+    if ((alg == "insert" || alg == "merge" || alg == "quick" || alg == "radix") && (seq == "")){
+        name.sort(alg);
     }else{ // alg == <sort>Ex
-        name.example(alg);
+        name.example(alg, seq);
     }
 }
